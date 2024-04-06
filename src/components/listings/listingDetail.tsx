a027d104a4f082c239c430ead7bd09a6f7e673b8
SamuelFlet <samuelrhfletcher@proton.me>
2024-04-06 18:16:01 -0400

    yup

--- src/components/listings/listingDetail.tsx
+++ src/components/listings/listingDetail.tsx
@@ -18,6 +18,7 @@ const listDetail = gql`
       }
       postedby {
         name
+        email
       }
     }
   }
@@ -36,12 +37,34 @@ export default function ListingDetail() {
 
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
-
+  var myLink = document.getElementById("yourLinkId");
+  var test = data.getListing.postedby.email;
+  myLink?.setAttribute("href", "mailto:" + test);
   return (
     <>
-      <h2>{data.getListing.title}</h2>
-      <h2>{data.getListing.description}</h2>
-      <h2>{data.getListing.product.name}</h2>
+      <div className="justify-center p-8 md:flex">
+        <img src={data.getListing.photo} className="md:w-1/3" alt="" />
+
+        <div className="flex pt-4 dark:text-white md:pl-4">
+          <div className="w-full">
+            <h2 className="text-2xl font-bold underline font-notnormal">
+              {data.getListing.title}
+            </h2>
+            <span className="">{data.getListing.description}</span>
+            <p className="text-sm">{data.getListing.product.name}</p>
+            <p className="mt-8 text-xl">${data.getListing.cost}</p>
+            <p className="mt-8 text-md">
+              Email the Seller: {data.getListing.postedby.name}
+            </p>
+            <a
+              className="inline-flex items-center px-4 py-3 mt-4 text-sm font-semibold text-white bg-blue-600 border border-transparent rounded-lg gap-x-2 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+              id="yourLinkId"
+            >
+              SEND EMAIL
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/products/productDetail.tsx
+++ src/components/products/productDetail.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { gql, useQuery } from "urql";
 import Rating from "react-rating";
-
+import { Link } from "react-router-dom";
 /**
  * GraphQL query to fetch product details and average rating based on the product ID.
  * @param {Int} $id - The ID of the product to fetch details for.
@@ -19,25 +19,20 @@ const prodDetail = gql`
   }
 `;
 
-/*const getListings = gql`
+const getListings = gql`
   query ($id: Int!) {
     prodlistFeed(id: $id, orderBy: { cost: desc }) {
-      cost
-      description
-      title
-      photo
       id
+      title
+      description
+      cost
+      photo
       postedby {
         name
       }
-      product {
-        name
-        category
-        id
-      }
     }
   }
-`;*/
+`;
 
 export default function ProdDetail() {
   const { state } = useLocation();
@@ -49,47 +44,78 @@ export default function ProdDetail() {
     variables: { id },
   });
 
-  /* const [result2] = useQuery({
-     query: getListings,
-     variables: { id },
-   });*/
+  const [result2] = useQuery({
+    query: getListings,
+    variables: { id },
+  });
 
   const { data, fetching, error } = result;
 
   if (fetching) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
 
-  console.log(data.avg);
   return (
-    <div className="justify-center p-8 md:flex">
-      <div className="md:w-[70%] lg:w-[40%]">
-        <img
-          src={data.getProduct.photo}
-          className="object-cover w-full"
-          alt=""
-        />
-      </div>
-      <div className="flex pt-4 dark:text-white md:pl-4">
-        <div className="w-full">
-          <h2 className="font-bold font-notnormal">{data.getProduct.name}</h2>
-          <span className="">{data.getProduct.category}</span>
-          <div className="flex w-full mt-8">
-            <div>
-              <div className="flex">
-                {/* @ts-expect-error Server Component */}
-                <Rating
-                  emptySymbol="fa fa-star-o fa-2x dark:text-white"
-                  fullSymbol="fa fa-star fa-2x dark:text-white"
-                  initialRating={data.avg._avg.rating.toFixed(2)}
-                  readonly
-                />
-                <p className="pl-2 dark:text-slate-300">
-                  ({data.avg._avg.rating.toFixed(2)})
-                </p>
+    <div>
+      <div className="justify-center p-8 md:flex">
+        <div className="md:w-[70%] lg:w-[40%]">
+          <img
+            src={data.getProduct.photo}
+            className="object-cover w-full"
+            alt=""
+          />
+        </div>
+        <div className="flex pt-4 dark:text-white md:pl-4">
+          <div className="w-full">
+            <h2 className="font-bold font-notnormal">{data.getProduct.name}</h2>
+            <span className="">{data.getProduct.category}</span>
+            <div className="flex w-full mt-8">
+              <div>
+                <div className="flex">
+                  {/* @ts-expect-error Server Component */}
+                  <Rating
+                    emptySymbol="fa fa-star-o fa-2x dark:text-white"
+                    fullSymbol="fa fa-star fa-2x dark:text-white"
+                    initialRating={data.avg._avg.rating.toFixed(2)}
+                    readonly
+                  />
+                  <p className="pl-2 dark:text-slate-300">
+                    ({data.avg._avg.rating.toFixed(2)})
+                  </p>
+                </div>
+                <p>based on {data.avg._count.rating} reviews</p>
               </div>
-              <p>based on {data.avg._count.rating} reviews</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="p-8">
+        <div className="mb-4 md:text-center">
+          <span className="font-bold dark:text-white">
+            View all Listings for {data.getProduct.name}
+          </span>
+        </div>
+        <div className="flex flex-col items-center">
+          {result2.data?.prodlistFeed.map((prods) => (
+            <div className="flex flex-col my-2 border border-black dark:border-white md:flex-row 2 md:max-w-lg rounded-xl">
+              <img src={prods.photo} className="max-w-xs rounded-l-xl" />
+              <div className="max-w-sm p-4 dark:text-white">
+                <p className="text-xl font-bold underline">{prods.title}</p>
+                <p className="text-lg">{prods.description}</p>
+                <p className="mt-2 font-bold underline">{prods.cost}</p>
+                <Link
+                  to={`/listings/${prods.id}`}
+                  key={prods.id}
+                  state={{ listingid: prods.id }}
+                  className="inline-flex items-center px-4 py-3 mt-4 text-sm font-semibold text-white bg-blue-600 border border-transparent rounded-lg gap-x-2 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  onClick={() => {
+                    window.scroll(0, 0);
+                  }}
+                >
+                  VIEW FULL LISTING
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
